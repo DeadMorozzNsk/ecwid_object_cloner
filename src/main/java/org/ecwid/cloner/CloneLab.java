2b66ea2afedb2f1db3909695ce6fcc984c5f66bb
DeadMorozz <evlampejev@gmail.com>
2020-12-28 15:34:01 +0400

    psvm sout correction

--- src/main/java/org/ecwid/cloner/CloneLab.java
+++ src/main/java/org/ecwid/cloner/CloneLab.java
@@ -11,6 +11,7 @@ public class CloneLab {
     public static void main(String[] args) {
         Man man1= new Man("John", 31, Arrays.asList("1984", "Effective Java. Third Edition"));
         Man man2 = Cloner.deepCopy(man1);
-        System.out.println(man1.equals(man2));
+        System.out.println("Objects are similar = " + man1.equals(man2));
+        System.out.println("It's the same object = " + (man1 == man2));
     }
 }
